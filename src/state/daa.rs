449632613eaf2f6db617241a9e55d2363c1cbfde
Axel Örn Sigurðsson <axel@absalon.is>
2018-07-16 02:45:37 +0900

    Implement simple version of DAA, ignoring AC flag

--- src/state/daa.rs
+++ src/state/daa.rs
@@ -3,6 +3,14 @@ use super::State;
 impl State {
     pub fn daa(&mut self) -> Option<u8> {
         // 4 cycles
-        panic!("DAA is not implemented");
+        if self.a & 0x0f > 9 {
+            self.a += 6;
+        }
+        if self.a & 0xf0 > 0x90 {
+            let (result, carry) = self.a.overflowing_add(0x60);
+            self.a = result;
+            self.set_flags(result, carry);
+        }
+        None
     }
 }
