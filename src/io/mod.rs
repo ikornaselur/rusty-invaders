feaedb68b78cc1c2b25c2548f88229564d92ae20
Axel Örn Sigurðsson <axel@absalon.is>
2018-07-16 02:44:34 +0900

    Implement IO shift

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,25 +1,85 @@
 #[derive(Debug, PartialEq)]
 pub struct IO {
     ports: Vec<u8>,
+    shift_offset: u8,
+    shift_value: u16,
 }
 
 impl IO {
     pub fn new(ports: usize) -> IO {
         IO {
             ports: vec![0; ports],
+            shift_offset: 0,
+            shift_value: 0,
         }
     }
 
     pub fn read(&self, port: usize) -> u8 {
-        println!("READING FROM IO PORT {}", port);
-        self.ports[port]
+        match port {
+            3 => (self.shift_value >> (8 - self.shift_offset)) as u8,
+            _ => self.ports[port],
+        }
+    }
+
+    pub fn set(&mut self, port: usize, value: u8) -> () {
+        self.ports[port] = value;
     }
 
     pub fn write(&mut self, port: usize, value: u8) -> () {
-        if port == 6 {
-            return (); // Debug port, ignore for now
+        match port {
+            2 => self.shift_offset = value & 0x7,
+            4 => self.shift_value = (self.shift_value >> 8) | (value as u16) << 8,
+            6 => (),
+            _ => self.ports[port] = value,
         }
-        println!("WRITING TO IO PORT {}: {:02X?}", port, value);
-        self.ports[port] = value;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn writing_port_4() {
+        let mut io = IO::new(8);
+
+        io.write(4, 0xAB);
+        assert_eq!(io.shift_value, 0xAB00);
+
+        io.write(4, 0xCD);
+        assert_eq!(io.shift_value, 0xCDAB);
+
+        io.write(4, 0xEF);
+        assert_eq!(io.shift_value, 0xEFCD);
+    }
+
+    #[test]
+    fn writing_port_2() {
+        let mut io = IO::new(8);
+
+        io.write(2, 1);
+        assert_eq!(io.shift_offset, 1);
+
+        io.write(2, 7);
+        assert_eq!(io.shift_offset, 7);
+
+        io.write(2, 8);
+        assert_eq!(io.shift_offset, 0);
+    }
+
+    #[test]
+    fn read_port_3() {
+        let mut io = IO::new(8);
+
+        io.shift_value = 0xDEAD;
+
+        io.shift_offset = 0;
+        assert_eq!(io.read(3), 0xDE);
+
+        io.shift_offset = 4;
+        assert_eq!(io.read(3), 0xEA);
+
+        io.shift_offset = 7;
+        assert_eq!(io.read(3), 0x56);
     }
 }
--- src/state/io.rs
+++ src/state/io.rs
@@ -3,13 +3,13 @@ use super::State;
 impl State {
     pub fn input(&mut self) -> Option<u8> {
         let port = self.read_byte().unwrap();
-        self.a = self.input.read(port as usize);
+        self.a = self.io.read(port as usize);
         None
     }
 
     pub fn output(&mut self) -> Option<u8> {
         let port = self.read_byte().unwrap();
-        self.output.write(port as usize, self.a);
+        self.io.write(port as usize, self.a);
         None
     }
 }
@@ -25,8 +25,8 @@ mod test {
             ..State::default()
         };
 
-        state.input.write(1, 0xDE);
-        state.input.write(2, 0xAD);
+        state.io.set(1, 0xDE);
+        state.io.set(2, 0xAD);
 
         state.input();
         assert_eq!(state.a, 0xDE);
@@ -36,18 +36,18 @@ mod test {
     }
 
     #[test]
-    fn output_writes_into_ouput_from_accumulator() {
+    fn output_writes_into_output_from_accumulator() {
         let mut state = State {
-            memory: vec![0x1, 0x2],
+            memory: vec![0x1, 0x1],
             ..State::default()
         };
 
         state.a = 0xDE;
         state.output();
+        assert_eq!(state.io.read(1), 0xDE);
+
         state.a = 0xAD;
         state.output();
-
-        assert_eq!(state.output.read(1), 0xDE);
-        assert_eq!(state.output.read(2), 0xAD);
+        assert_eq!(state.io.read(1), 0xAD);
     }
 }
