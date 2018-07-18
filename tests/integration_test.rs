13220ea2964548e071c6c4fadfe831f8f1818c95
Axel Örn Sigurðsson <axel@absalon.is>
2018-07-18 15:50:51 +0900

    Update integration test to just use the cpu State

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,16 +1,24 @@
 extern crate rusty_invaders;
 
-use rusty_invaders::Config;
+use std::fs::File;
+use std::io::prelude::*;
+
+use rusty_invaders::state::State;
 
 #[test]
 fn full_cpu_test() {
-    let config = Config {
-        filename: String::from("tests/cpu.bin"),
-        debug: true,
-    };
+    let mut f = File::open("tests/cpu.bin").expect("Failed to open cpu.bin");
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer)
+        .expect("Failed to read cpu.bin into buffer");
+    buffer.resize(0x10000, 0);
 
-    assert!(
-        !rusty_invaders::run(config).is_err(),
-        "Error running full cpu test"
-    );
+    let mut state = State::new(buffer, true);
+
+    loop {
+        match state.step() {
+            None => break,
+            _ => (),
+        }
+    }
 }
